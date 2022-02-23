c8415065e01fc6ffe9382b64623449242506d258
Nikhil Jain <jainnikhil472@gmail.com>
2022-02-23 17:48:38 +0530

    Complete send email

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Form } from "react-bootstrap";
+import emailjs from "emailjs-com";
 function App() {
+  const sendEmail = (e) =>{
+    e.preventDefault();
+    //Here we pass the 4 parameter
+    emailjs.sendForm('service_lpbyand','template_ptqan4u',e.target,'user_xBPeSp7lKBypcyF0SVEHC').then(res => {
+      console.log(res)
+    }).catch(err =>{
+      console.log(err)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="container border"
+      style={{
+        marginTop: "50px",
+        width: "50%"
+      }}
+    >
+      <h1 className="heading">Send Email</h1>
+      <Form className="row" style={{margin:"25px 85px 75px 100px"}} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="name" className="form-control" />
+          <br />
+          <br />
+
+          <label>Email</label>
+          <input type="email" name="user_email" className="form-control"/>
+          <br /><br />
+
+          <label>Message</label>
+          <textarea name="message" rows="4" className="form-control"/>
+          <br />
+          <br />
+
+          <input type="submit" value="send" />
+      </Form>
     </div>
   );
 }
